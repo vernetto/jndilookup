f00d2a6b532ae1ffc06b6f434f3e9dbb004024a0
vernetto <Ernesto0z>
2023-02-11 17:06:01 +0100

    one

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+	@Autowired
+	@Qualifier("one")
+	PropertyHolder theProperty;
+
+	@Autowired
+	@Qualifier("two")
+	PropertyHolder theProperty2;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(theProperty);
+		System.out.println(theProperty2);
+	}
+}
---
+++ src/main/java/com/example/demo/MyConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource(
+        {
+                "classpath:spring-beans.xml"
+        }
+)
+public class MyConfiguration {
+}
